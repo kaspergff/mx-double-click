5a2d09c34b0b2d477c8f1801d70adbcb79ebe80a
Nathan Ruth | ITvisors <nathan.ruth@itvisors.com>
2021-06-28 17:26:35 +0200

    Initial commit

---
+++ src/DoubleClick.jsx
@@ -0,0 +1,35 @@
+import { Component, createElement } from "react";
+
+import "./ui/DoubleClick.css";
+
+export default class DoubleClick extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.onDoubleClick = this.onDoubleClick.bind(this);
+    }
+
+    onDoubleClick() {
+        if (this.props.dblClickAction && this.props.dblClickAction.canExecute) {
+            this.props.dblClickAction.execute();
+        }
+    }
+
+    render() {
+        var classes = "widget-doubleclick";
+        if (this.props.showPointer) {
+            classes += " widget-doubleclick-clickable";
+        }
+        return (
+            <div
+                className={classes}
+                onDoubleClick={this.onDoubleClick}
+            >
+                {this.props.children}
+            </div>
+        );
+    }
+
+
+}
